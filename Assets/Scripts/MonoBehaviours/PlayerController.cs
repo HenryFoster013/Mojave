7eec783f3db3bd8842b9d74040760520740ce01c
Henry Foster <henryfoster013@gmail.com>
2026-01-30 17:52:18 +0000

    Working on mouse controls

--- Assets/Scripts/MonoBehaviours/PlayerController.cs
+++ Assets/Scripts/MonoBehaviours/PlayerController.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static SoundUtils;
+using RISK_Utils;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Internal References")]
+    [SerializeField] Camera Cam;
+    
+    [Header("External References")]
     [SerializeField] MapManager Map;
     [SerializeField] SoundEffectLookupSO SoundEffectLookup;
      
@@ -15,5 +20,14 @@ public class PlayerController : MonoBehaviour
     void ManageKeyboardInputs(){
         if(Input.GetKeyDown("k"))
             Map.FlipRenderMode();
+    }
+
+    public void Select(TerritoryInstance territory){
+        if(territory == null)
+            Deselect();
+    }
+
+    public void Deselect(){
+
     }
 }
